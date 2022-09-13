4634f63ffcb19d59ed1816d601bfb91dab62b55e
Jas <doggrell@gmail.com>
2022-09-13 18:46:32 +0100

    Correcly adding a spare and very simple single strike condition

--- BowlingGame.Tests/CheckTheScores.cs
+++ BowlingGame.Tests/CheckTheScores.cs
@@ -19,9 +19,40 @@ public class TestTheScores
     }
 
     [Test]
-    public void Simple_Addition_Of_With_Dash()
+    public void Simple_Addition_Of_Integers_With_Dash()
     {
         char[] scoreWithDash = { '1', '2', '-' };
         _bowlingScores.CalculateScores(scoreWithDash).Should().Be(3);
     }
+
+    [Test]
+    public void Adding_With_Ending_With_A_Spare()
+    {
+        char[] scoreWithSpare = { '1', '2', '/' };
+        _bowlingScores.CalculateScores(scoreWithSpare).Should().Be(13);
+    }
+
+    [Test]
+    public void Adding_With_A_Spare()
+    {
+        char[] testFrames = { '1', '/', '2' };
+        _bowlingScores.CalculateScores(testFrames).Should().Be(13);
+    }
+
+    [Test]
+    public void Adding_With_Strike_With_Two_Frames_Left() 
+    {
+         char[] testFrames = { 'X', '1', '2' };
+        _bowlingScores.CalculateScores(testFrames).Should().Be(16);
+
+       // char[] testFrames2 = { 'X', 'X', '4', '2' };
+       // _bowlingScores.CalculateScores(testFrames2).Should().Be(46);
+    }
+
+    /* [Test]
+     public void Adding_With_Ending_A_Strike() //fails - need to check that index is valid
+     {
+         char[] scoreWithDash = { '1', '2', 'X' };
+         _bowlingScores.CalculateScores(scoreWithDash).Should().Be(13);
+     }*/
 }
--- BowlingGame/BowlingScores.cs
+++ BowlingGame/BowlingScores.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,16 +9,30 @@ namespace BowlingGame
 {
     public class BowlingScores
     {
+        private const char MISS = '-';
+        private const char SPARE = '/';
+        private const char STRIKE = 'X';
+        private const int TEN = 10;
+
         public int CalculateScores(char[] input)
         {
             int score = 0;
 
-            foreach (char c in input)
+            //foreach (char c in input)
+            foreach (var c in input.Select((value, i) => (value, i)))
             {
-                if (Char.IsDigit(c)) // Ignore X,/.-
+                if (Char.IsDigit(c.value)) // Ignore X,/.-
                 {                   
-                    score += Convert.ToInt32(Char.GetNumericValue(c)); 
-                }                
+                    score += Convert.ToInt32(Char.GetNumericValue(c.value)); 
+                }
+                if (c.value.Equals(SPARE)) // Add 10 and next frame
+                {
+                    score += 10;
+                }
+                if (c.value.Equals(STRIKE)) // Add 10 and next two frames
+                {
+                    score += 10 + Convert.ToInt32(Char.GetNumericValue(input[c.i+1])) + Convert.ToInt32(Char.GetNumericValue(input[c.i + 2])); 
+                }
             }
             return score;
         }
